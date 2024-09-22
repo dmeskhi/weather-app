7cf881ba20db0eccd3733e2296e6b2988dc2dea9
David Meskhishvili <dmeskh@gmail.com>
2024-09-21 20:11:32 -0700

    Search weather by zip code

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const apiKey = "e66d155c3c4545d2482cee8a8ce48655";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+async function checkWeather(zip) {
+  const response = await fetch(apiUrl + zip + `&appid=${apiKey}`);
 
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
@@ -14,7 +14,7 @@ async function checkWeather(city) {
   } else {
     let data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".zip").innerHTML = data.name;
     document.querySelector(".country").innerHTML = data.sys.country;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "F";
